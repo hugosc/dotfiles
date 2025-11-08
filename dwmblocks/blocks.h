34ba10650deadaa1662b2b52ea3e552db0ecbfdb
Crocodile <hugo.serranochan@gmail.com>
2025-11-08 21:06:24 +0000

    fix: weather intervals

--- dwmblocks/blocks.h
+++ dwmblocks/blocks.h
@@ -4,7 +4,7 @@ static const Block blocks[] = {
 	// {"", "~/.config/scripts/statusbar/colorscheme",			0,		8},
 	{"", "~/.config/scripts/statusbar/screenrecord",			1,		12},
 	{"", "~/.config/scripts/statusbar/musicplaying",			0,		11},
-	{"", "~/.config/scripts/statusbar/weather",				30,		0},
+	{"", "~/.config/scripts/statusbar/weather",				1200,		0},
 	{"", "~/.config/scripts/statusbar/wifi",				30,		0},
 	{"", "~/.config/scripts/statusbar/battery",				120,		0},
 	{"", "~/.config/scripts/statusbar/timedate",			60,		0},
